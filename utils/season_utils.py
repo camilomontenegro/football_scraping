e401960664f519faa4154590dc46ebb60dd7129d
alvaro <alvarolassaletta@gmail.com>
2026-05-13 16:48:49 +0200

    feat: add utils to normalize dim_match.season & coordinates to format 1-0

--- utils/season_utils.py
+++ utils/season_utils.py
@@ -4,26 +4,42 @@ import re
 
 def normalize_season(raw_season: str) -> str:
     """
-    Normaliza cualquier formato de temporada a 'YY/YY'.
-    En algunos CSV  lso datos de la temporada para el campo season de DIM_MATCH  
-    vienen con texto que no interesa conservar  en los registros 
-    Hay que limpiar  el dato y quedarse solo  con la temporada, que es el dato que interesa. 
+    Normaliza cualquier formato de temporada a 'YYYY/YYYY'.
+    En algunos CSV  los datos de la temporada para el campo season de DIM_MATCH  
+    vienen con texto que no interesa conservar  en los registros. Hay que limpiar  el dato y quedarse solo  con la temporada, que es el dato que interesa. 
+    También pueden venir con formato distinto a YYYY/YYYY
 
     Ejemplos:
-        "UEFA Champions League 25/26" → "25/26"
-        "LaLiga 20/21"                → "20/21"
-        "2020/2021"                   → "20/21"
-        "2020/21"                     → "20/21"
-        "20/21"                       → "20/21"
+        Normaliza cualquier formato de temporada a 'YYYY/YYYY'.
+
+    Ejemplos:
+        "LaLiga 20/21"                → "2020/2021"
+        "20/21"                       → "2020/2021"
+        "2020/2021"                   → "2020/2021"
+        "2020/21"                     → "2020/2021"
+        "2021"                        → "2021/2022"
+        "UEFA Champions League 25/26" → "2025/2026"
+       
     """
-    
+
     if not raw_season or not isinstance(raw_season, str):
-        return None  # devuelve None en lugar de string vacío para que la BD lo trate como NULL
+        return None
 
-    match = re.search(r'(\d{2,4})/(\d{2,4})', raw_season)
-    if not match:
-        return None  # si no encuentra el patrón YY/YY devuelve None
+    # Caso año suelto: "2021" → "2021/2022"
+    # Este es el supuesto de understat 
+    solo_year = re.match(r'^(\d{4})$', raw_season.strip())
+    if solo_year:
+        year = int(solo_year.group(1))
+        return f"{year}/{year + 1}"
 
-    start = match.group(1)[-2:]
-    end   = match.group(2)[-2:]
-    return f"{start}/{end}"
+    m = re.search(r'(\d{2,4})/(\d{2,4})', raw_season)
+    if not m:
+        return None
+
+    start = m.group(1)
+
+    # Si start es YY, expandir a YYYY asumiendo siglo 21
+    if len(start) == 2:
+        start = "20" + start
+
+    return f"{start}/{int(start) + 1}"
